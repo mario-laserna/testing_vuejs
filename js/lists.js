00c4bb5b512343fe67ad111c0a2486a1ebcc985b
mario <laserna.mario@gmail.com>
2016-04-20 20:54:29 -0500

    listas

--- js/lists.js
+++ js/lists.js
@@ -1,6 +1,31 @@
 Vue.component('tasks', {
     template: '#tasks-template',
-    props: ['list']
+
+    props: ['list'],
+
+    computed: {
+        remaining: function () {
+            return this.list.filter(this.isProgress).length;
+        }
+    },
+
+    methods: {
+        isCompleted: function (task) {
+            return task.completed;
+        },
+
+        isProgress: function (task) {
+            return !this.isCompleted(task);
+        },
+
+        deleteTask: function (task) {
+            this.list.$remove(task);
+        },
+        
+        clearCompleted: function () {
+            this.list = this.list.filter(this.isProgress);
+        }
+    }
 });
 
 new Vue({
